4c4d7d812800ea8a599aa74995d20bb5512d1f9f
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-07-12 11:22:17 -0400

    Modernize package

--- Package.swift
+++ Package.swift
@@ -1,28 +1,24 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
-    name: "KeyCodes",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "KeyCodes",
-            targets: ["KeyCodes"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "KeyCodes",
-            dependencies: []),
-        .testTarget(
-            name: "KeyCodesTests",
-            dependencies: ["KeyCodes"]),
-    ]
+	name: "KeyCodes",
+	products: [
+		.library(name: "KeyCodes", targets: ["KeyCodes"]),
+	],
+	targets: [
+		.target(name: "KeyCodes"),
+		.testTarget(name: "KeyCodesTests", dependencies: ["KeyCodes"]),
+	]
 )
+
+let swiftSettings: [SwiftSetting] = [
+	.enableExperimentalFeature("StrictConcurrency"),
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}
